762e383bbb4ac0b24a27736fead59de1dad05377
Barbod Habibi <barbod@kpmpower.com>
2025-07-10 09:59:05 -0400

    Set up project container

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Tool from "$lib/components/common/Tool.svelte";
+	import Project from "./Project.svelte";
+</script>
+
+<section>
+	<h1>Projects</h1>
+
+	<article>
+		<div class="d-flex">
+			<h5><strong>Solve Me!</strong></h5>
+			<a href="https://github.com/bitstachio/solve-me">GitHub</a>
+			<!-- Replace with GitHub icon -->
+		</div>
+		<ul>
+			<li>Bullet point #1</li>
+			<li>Bullet point #2</li>
+			<li>Bullet point #3</li>
+		</ul>
+		<section>
+			<!-- Skills container -->
+			<Tool title="JavaScript" imgSrc="" backgroundColor="yellow" />
+			<Tool title="Node.js" imgSrc="" backgroundColor="yellow" />
+		</section>
+	</article>
+
+	<section class="container">
+		<div class="row">
+			<div class="col-12 col-sm-6 col-md-3">
+				<Project />
+			</div>
+			<div class="col-12 col-sm-6 col-md-3">Item 2</div>
+			<div class="col-12 col-sm-6 col-md-3">Item 3</div>
+			<div class="col-12 col-sm-6 col-md-3">Item 4</div>
+			<div class="col-12 col-sm-6 col-md-3">Item 5</div>
+		</div>
+	</section>
+</section>
