a14e9c5fa94b2c22aab3413478f78edcba393b18
Barbod Habibi <barbod@kpmpower.com>
2025-07-10 09:59:05 -0400

    Set up project container

--- src/lib/components/common/Tool.svelte
+++ src/lib/components/common/Tool.svelte
@@ -1,1 +1,20 @@
-<span class="badge rounded-pill bg-primary">C#</span>
+<script lang="ts">
+	interface Props {
+		title: string;
+		imgSrc: string;
+		backgroundColor?: string;
+		textColor?: string;
+	}
+
+	let { title, imgSrc, backgroundColor, textColor }: Props = $props();
+</script>
+
+<span
+	class="badge rounded-pill"
+	style="background-color: {backgroundColor ?? 'var(--bs-primary)'}; color: {textColor ?? 'white'}"
+>
+	<div class="d-flex gap-1">
+		<img src={imgSrc} alt="{title} icon" />
+		<p>{title}</p>
+	</div>
+</span>
---
+++ src/routes/projects/Project.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Tool from "$lib/components/common/Tool.svelte";
+</script>
+<div class="card mb-3">
+	<h3 class="card-header">Mad Grid</h3>
+	<div class="card-body pb-0">
+		<ul>
+			<li>Description #1</li>
+			<li>Description #2</li>
+			<li>Description #3</li>
+		</ul>
+	</div>
+	<div class="card-footer">
+        <Tool title={"JavaScript"} imgSrc={""} />
+    </div>
+</div>
