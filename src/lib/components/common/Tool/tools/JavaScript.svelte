964db4bad16370394800f4d593dd03a11ee9011c
Barbod Habibi <barbod@kpmpower.com>
2025-07-10 21:26:12 -0400

    Set up basic dynamic tool item

---
+++ src/lib/components/common/Tool/tools/JavaScript.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Tool from "../Tool.svelte";
+</script>
+
+<Tool title={"JavaScript"} imgSrc={"$lib/assets/dev-icons/javascript-original.svg"} backgroundColor={"yellow"} />
