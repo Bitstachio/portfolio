b60ac18e3ee6a5f19fd9e93f590b561673ca7ad5
BarbodH <habibi.k.barbod@gmail.com>
2025-07-13 16:01:25 -0400

    Set up GitHub links

--- src/routes/projects/Project.svelte
+++ src/routes/projects/Project.svelte
@@ -6,18 +6,28 @@
 		title: string;
 		description: string[];
 		tools: IToolProps[];
+		linkGitHub?: string;
 	}
 
-	let { title, description, tools }: Props = $props();
+	let { title, description, tools, linkGitHub }: Props = $props();
 </script>
 
 <div class="card mb-3">
-	<h3 class="card-header"><strong>{title}</strong></h3>
-	<div class="card-body pb-0">
-		<ul>
-			{#each description as bulletPoint}
-				<li>{bulletPoint}</li>
-			{/each}
+	<div class="card-header d-flex justify-content-between">
+		<h3><strong>{title}</strong></h3>
+		<div class="d-flex align-items-center gap-3">
+            {#if linkGitHub}
+                <a href={linkGitHub} target="_blank">
+                    <img src="src/lib/assets/dev-icons/github-original.svg" alt="GitHub icon" height="28" />
+                </a>
+            {/if}
+        </div>
+    </div>
+    <div class="card-body pb-0">
+        <ul>
+            {#each description as bulletPoint}
+                <li>{bulletPoint}</li>
+            {/each}
 		</ul>
 	</div>
 	<div class="card-footer">
