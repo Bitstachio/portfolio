95f6ecfe1d790f7f5d8bbb01f5192d45b5531054
BarbodH <habibi.k.barbod@gmail.com>
2025-07-13 18:27:16 -0400

    Add itch icon/link + fix ESLint problems

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Project from "./Project.svelte";
-	import {projectConfigs} from "./project.constants";
+	import { projectConfigs } from "./project.constants";
 </script>
 
 <section>
@@ -9,12 +9,7 @@
 		<div class="row">
 			{#each projectConfigs as config (config.title)}
 				<div class="col-12 col-md-4">
-					<Project
-						title={config.title}
-						description={config.description}
-						tools={config.tools}
-						links={config.links}
-					/>
+					<Project title={config.title} description={config.description} tools={config.tools} links={config.links} />
 				</div>
 			{/each}
 		</div>
