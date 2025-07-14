800d8952b7183b275aee26a2b0dc2f86b81d8b33
BarbodH <habibi.k.barbod@gmail.com>
2025-07-14 06:05:47 -0400

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,30 +2,32 @@
 	<title>Homepage</title>
 </svelte:head>
 
-<section class="banner">
-	<h1>Hey, I'm Barbod</h1>
-	<p>
-		I'm a 4th-year <strong>Computer Science</strong> student at York University, interested in
-		<strong>full-stack development</strong>
-		and <strong>physics engine programming</strong>.
-	</p>
-	<p>
-		Currently, I'm working as a Software Engineer at <a
-			href="https://kpmpower.com"
-			target="_blank"
-			rel="noopener noreferrer">KPM Power</a
-		>
-	</p>
+<section class="banner d-flex flex-column justify-content-center text-center">
+	<div class="bg-white bg-opacity-75 p-4 rounded shadow w-75 mx-auto">
+		<h1>Hey, I'm Barbod 👋</h1>
+		<br>
+		<p>
+			I'm a 4th-year <strong>Computer Science</strong> student at York University, interested in
+			<strong>full-stack development</strong>
+			and <strong>physics engine programming</strong>.
+		</p>
+		<p>
+			Currently, I'm working as a Software Engineer at <a
+				href="https://kpmpower.com"
+				target="_blank"
+				rel="noopener noreferrer">KPM Power</a
+			>
+		</p>
+	</div>
 </section>
 
 <style>
 	.banner {
-		padding-top: 10rem;
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
+		background-image: url("page/home.jpg");
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		height: 100vh;
+		padding: 4rem 0;
 	}
 </style>
