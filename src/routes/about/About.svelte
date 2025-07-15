b86a185418185b3dbf6670c89426c0b7afad6f59
Barbod Habibi <barbod@kpmpower.com>
2025-07-14 20:08:36 -0400

    Set up about article component

--- src/routes/about/About.svelte
+++ src/routes/about/About.svelte
@@ -1,3 +1,23 @@
 <script lang="ts">
+	interface Props {
+		title: string;
+		passage: string;
+		imgSrc: string;
+		imgAlt?: string;
+	}
 
+	let { title, passage, imgSrc, imgAlt }: Props = $props();
 </script>
+
+<article
+	class="d-flex justify-content-center align-items-center w-75 mx-auto border border-primary border-2 rounded shadow-lg"
+>
+	<div class="p-5">
+		<h3>{title}</h3>
+		<br />
+		<p>{passage}</p>
+	</div>
+	<div class="p-3 bg-primary">
+		<img src={imgSrc} alt={imgAlt ?? title} height="300" />
+	</div>
+</article>
