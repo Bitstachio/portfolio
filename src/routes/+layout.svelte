b952a537ae75aaf23f749dcdebf1676f1af006ce
BarbodH <habibi.k.barbod@gmail.com>
2025-07-20 00:56:50 -0400

    Incorporate background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import "bootswatch/dist/flatly/bootstrap.min.css";
 	import "$lib/styles/global.scss";
+	import "$lib/styles/bootstrap-extension.scss";
 	import Navbar from "$lib/components/layout/Navbar.svelte";
 </script>
 
