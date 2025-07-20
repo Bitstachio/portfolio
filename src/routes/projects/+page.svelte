468a39a0976058966ba14b01b0f3af875a9c6bf8
BarbodH <habibi.k.barbod@gmail.com>
2025-07-20 11:52:33 -0400

    Update container backgrounds

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import Project from "./Project.svelte";
 	import { projectConfigs } from "./project.constants";
+	import SectionTitle from "$lib/components/layout/SectionTitle.svelte";
 </script>
 
 <section class="app-container-main">
-	<h1>Projects</h1>
+	<SectionTitle text="Projects" />
 	<section class="container">
 		<div class="row">
 			{#each projectConfigs as config (config.title)}
