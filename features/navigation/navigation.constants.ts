e6f9bb11fe91d5e0c65b445a5348f1bec1fcaa7e
BarbodH <habibi.k.barbod@gmail.com>
2025-12-28 12:10:25 -0500

    Init Next.js app with Tailwind & Prettier

---
+++ features/navigation/navigation.constants.ts
@@ -0,0 +1,9 @@
+import { TNavConfig } from "./navigation.types";
+
+export const navbarConfig: TNavConfig = [
+  {
+    id: "home",
+    label: "Home",
+    href: "/",
+  },
+];
--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import "bootswatch/dist/flatly/bootstrap.min.css";
-	import "$lib/styles/global.scss";
-	import "$lib/styles/bootstrap-extension.scss";
-	import Navbar from "$lib/components/layout/Navbar.svelte";
-</script>
-
-<Navbar />
-<slot />
