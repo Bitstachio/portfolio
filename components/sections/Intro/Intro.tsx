c4f4f0468026d1027bdcdcf3aae8f78ebcea72e1
BarbodH <habibi.k.barbod@gmail.com>
2025-12-28 15:19:40 -0500

    Set up basic intro content & new font

---
+++ components/sections/Intro/Intro.tsx
@@ -0,0 +1,9 @@
+const Intro = () => (
+  <header>
+    <h1 className="text-5xl font-bold tracking-tighter">Barbod Habibi</h1>
+    <p className="text-xl font-semibold mt-1">Software Engineer</p>
+    <p className="mt-3">I build something...</p>
+  </header>
+);
+
+export default Intro;
