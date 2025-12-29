9aa11f59282ffe3aac9d6e102ac3061a13ce411d
BarbodH <habibi.k.barbod@gmail.com>
2025-12-29 01:27:33 -0500

    Style experience card

--- components/sections/experience/ExperienceCard/ExperienceCard.tsx
+++ components/sections/experience/ExperienceCard/ExperienceCard.tsx
@@ -10,7 +10,12 @@ type TExperienceCardProps = {
 };
 
 const ExperienceCard = ({ title, company, description, startDate, endDate, url }: TExperienceCardProps) => (
-  <article className="relative mx-auto max-w-3xl">
+  <article
+    className="
+      group relative mx-auto max-w-3xl p-5 rounded-lg transition-all duration-300 hover:bg-white/2
+      border-t border-transparent hover:border-slate-500/20
+    "
+  >
     <a
       href={url}
       target="_blank"
@@ -21,15 +26,15 @@ const ExperienceCard = ({ title, company, description, startDate, endDate, url }
 
     <div className="flex gap-6">
       <div className="w-42 flex-shrink-0">
-        <p>
-          {dateFormatter.format(startDate)} &ndash; {dateFormatter.format(endDate)}
+        <p className="text-xs text-subtle font-semibold">
+          {dateFormatter.format(startDate)} &ndash; {endDate ? dateFormatter.format(endDate) : "Present"}
         </p>
       </div>
 
       <div>
-        <h3>{title}</h3>
-        <p>{company}</p>
-        <p>{description}</p>
+        <h3 className="group-hover:text-accent duration-300 font-bold">{title}</h3>
+        <p className="text-subtle font-semibold">{company}</p>
+        <p className="text-muted mt-2">{description}</p>
       </div>
     </div>
   </article>
