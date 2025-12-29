58b729d02743f6040b7834565deaf54a31eaa608
BarbodH <habibi.k.barbod@gmail.com>
2025-12-29 15:53:04 -0500

    Set up basic responsive layout to handle wide screens

---
+++ src/components/sections/About/About.tsx
@@ -0,0 +1,10 @@
+const About = () => (
+  <article className="text-muted">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus veritatis explicabo ab et quod necessitatibus
+    fuga iusto repudiandae adipisci ex. Illum facere, dolore fugit mollitia esse reiciendis quas voluptatibus voluptate
+    nemo corrupti expedita animi tenetur laudantium ad vel quidem atque voluptas libero odit enim corporis, praesentium
+    officiis nostrum? Voluptatum, quam.
+  </article>
+);
+
+export default About;
--- src/components/sections/Intro/Intro.tsx
+++ src/components/sections/Intro/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => (
   <header>
     <h1 className="text-5xl font-bold tracking-tighter">Barbod Habibi</h1>
     <p className="text-xl font-semibold mt-1">Software Engineer</p>
-    <p className="mt-3">I build something...</p>
+    <p className="text-muted mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, sequi?</p>
   </header>
 );
 
