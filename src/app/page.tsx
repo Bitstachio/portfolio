1d134f0fb384394cd0fa1318643f63f37442ec95
BarbodH <habibi.k.barbod@gmail.com>
2025-12-29 15:53:04 -0500

    Set up basic responsive layout to handle wide screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,23 @@ import Connect from "../components/sections/Connect/Connect";
 import Experience from "../components/sections/Experience/Experience";
 import Intro from "../components/sections/Intro/Intro";
 import Navbar from "@/components/sections/Navbar/Navbar";
+import About from "@/components/sections/About/About";
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <Intro />
-        <Navbar />
-        <Connect />
-      </header>
-      <section>
-        <Experience />
-        <Projects />
-      </section>
-    </main>
+    <div className="px-6 py-12 max-w-screen-xl mx-auto">
+      <div className="flex justify-between gap-4 md:py-12">
+        <header className="w-1/2 max-w-sm md:ps-5">
+          <Intro />
+          <Navbar />
+          <Connect />
+        </header>
+        <main className="flex flex-col gap-16 w-1/2">
+          <About />
+          <Experience />
+          <Projects />
+        </main>
+      </div>
+    </div>
   );
 }
--- src/components/sections/Navbar/Navbar.tsx
+++ src/components/sections/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => (
         <li key={navItem.id}>
           <a
             href={navItem.href}
-            className="group flex items-center gap-3 p-1 transition-all font-semibold text-subtle hover:text-foreground"
+            className="group flex items-center gap-3 p-1 transition-all font-semibold text-subtle hover:text-foreground text"
           >
             <span className="inline-block h-px w-8 group-hover:w-16 bg-gray-400/50 group-hover:bg-gray-400 duration-300" />
             {navItem.label}
