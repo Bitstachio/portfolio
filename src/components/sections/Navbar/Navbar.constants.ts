1c1a4f1cf2b3a92e4484b59922d5a20cdae15acb
BarbodH <habibi.k.barbod@gmail.com>
2025-12-29 06:44:44 -0500

    Set up basic navigation structure & hover animation

---
+++ src/components/sections/Navbar/Navbar.constants.ts
@@ -0,0 +1,19 @@
+import { TNavItem } from "./Navbar.types";
+
+export const navItems = [
+  {
+    id: "about",
+    label: "About",
+    href: "#about",
+  },
+  {
+    id: "experience",
+    label: "Experience",
+    href: "#experience",
+  },
+  {
+    id: "projects",
+    label: "Projects",
+    href: "#projects",
+  },
+] satisfies TNavItem[];
