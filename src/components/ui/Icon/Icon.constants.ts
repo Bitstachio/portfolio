5759cd88aa52eec184f7530c215b3f6fc59026ea
BarbodH <habibi.k.barbod@gmail.com>
2025-12-29 20:32:10 -0500

    Set up general icon component

---
+++ src/components/ui/Icon/Icon.constants.ts
@@ -0,0 +1,7 @@
+import ReactIcon from "@/assets/icons/react.svg";
+import TypeScriptIcon from "@/assets/icons/typescript.svg";
+
+export const icons = {
+  typescript: TypeScriptIcon,
+  react: ReactIcon,
+} as const;
---
+++ src/components/ui/Icon/Icon.types.ts
@@ -0,0 +1,3 @@
+import { icons } from "./Icon.constants";
+
+export type TIconName = keyof typeof icons;
